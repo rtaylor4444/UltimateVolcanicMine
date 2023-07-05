74c1332497b55bf0dc597fc49a5422cb459e9f6b
rtaylor4444 <rtaylor4444@gmail.com>
2023-07-05 02:40:05 -0400

    pickaxe protection is now added

--- src/main/java/com/ultimatevm/VMNotifier.java
+++ src/main/java/com/ultimatevm/VMNotifier.java
@@ -1,5 +1,6 @@
 package com.ultimatevm;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import net.runelite.client.Notifier;
 
@@ -13,12 +14,16 @@ public class VMNotifier {
         VM_RESET,
         VM_ERUPTION,
         VM_PRE_RESET_VENT_FIX,
-        VM_PREDICTED_VENT_FIX;
+        VM_PREDICTED_VENT_FIX,
+        VM_PICKAXE_DESPAWN,
+        VM_PICKAXE_LOW_STABILITY,
+        VM_PICKAXE_BOULDER_COMPLETE;
     }
     public static final int NOTIFICATION_START_COOLDOWN_TICKS = 10;
     @Inject
     private UltimateVolcanicMineConfig config;
     private HashSet<NotificationEvents> oneTimeEvents = new HashSet<>();
+    private HashMap<NotificationEvents, Integer> continousEvents = new HashMap<>();
     @Inject
     VMNotifier(UltimateVolcanicMineConfig config) {
         this.config = config;
@@ -31,9 +36,16 @@ public class VMNotifier {
         oneTimeEvents.add(NotificationEvents.VM_ERUPTION);
         oneTimeEvents.add(NotificationEvents.VM_PRE_RESET_VENT_FIX);
         oneTimeEvents.add(NotificationEvents.VM_PREDICTED_VENT_FIX);
+        oneTimeEvents.add(NotificationEvents.VM_PICKAXE_DESPAWN);
+        oneTimeEvents.add(NotificationEvents.VM_PICKAXE_LOW_STABILITY);
+        oneTimeEvents.add(NotificationEvents.VM_PICKAXE_BOULDER_COMPLETE);
+    }
+    private void setContinousEvents() {
+        continousEvents.put(NotificationEvents.VM_PICKAXE_DESPAWN, 0);
     }
     public void reset() {
         setOneTimeEvents();
+        setContinousEvents();
     }
     public void removeEvent(NotificationEvents event) {
         oneTimeEvents.remove(event);
@@ -77,6 +89,24 @@ public class VMNotifier {
                 oneTimeEvents.remove(event);
                 if(!config.predictedVentFixNotifier()) return;
                 notifier.notify("Be alert you might have to fix your vent soon!");
+                break;
+
+            case VM_PICKAXE_DESPAWN:
+                int lastEventTime = continousEvents.get(NotificationEvents.VM_PICKAXE_DESPAWN);
+                if(lastEventTime + NOTIFICATION_START_COOLDOWN_TICKS < ticksPassed) {
+                    notifier.notify("Be careful your pickaxe will despawn soon!");
+                    continousEvents.put(NotificationEvents.VM_PICKAXE_DESPAWN, ticksPassed);
+                }
+                break;
+
+            case VM_PICKAXE_LOW_STABILITY:
+                oneTimeEvents.remove(event);
+                notifier.notify("Mine Stability is low don't forget your pickaxe!");
+                break;
+
+            case VM_PICKAXE_BOULDER_COMPLETE:
+                oneTimeEvents.remove(event);
+                notifier.notify("You finished the boulder, pick up your pickaxe!");
                 break;
 
             default:
