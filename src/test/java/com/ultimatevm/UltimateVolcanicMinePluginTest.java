16f2d4a3a6355dd4d3a6db6914ca3404e9c5e92f
Felanbird <41973452+Felanbird@users.noreply.github.com>
2024-12-02 13:10:04 -0500

    chore: revert `vmrecolor` changes

--- src/test/java/com/ultimatevm/UltimateVolcanicMinePluginTest.java
+++ src/test/java/com/ultimatevm/UltimateVolcanicMinePluginTest.java
@@ -1,6 +1,5 @@
 package com.ultimatevm;
 
-import com.vmrecolor.VMRecolorPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
@@ -8,7 +7,7 @@ public class UltimateVolcanicMinePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(UltimateVolcanicMinePlugin.class, VMRecolorPlugin.class);
+		ExternalPluginManager.loadBuiltin(UltimateVolcanicMinePlugin.class);
 		RuneLite.main(args);
 	}
 }
